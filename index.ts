8d4ab216bb48519da00da277c17e2d404f838265
Anosha Fatima <162595951+Anosha804@users.noreply.github.com>
2024-04-18 11:02:57 -0700

    Push my word counter app to GitHub

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.red.bold("\n \t MY WORD COUNTER APPLICATION \n"));
+
+const answer:{
+    sentence: string
+} = await inquirer.prompt([
+    {
+        name : "sentence",
+        type : "input",
+        message : "Enter your sentence to count the words:  "
+    }
+]);
+
+const words = answer.sentence.trim().split(" ");
+
+console.log(words);
+
+console.log(chalk.green.bold(`Your sentence word count is: ${words.length}`));
